699df649974c4f0b40b89b6aa20605dadc96d5e4
ccormon <camille.cormon@gmail.com>
2024-06-21 21:21:35 +0200

    tapas le chat

--- src/raycasting.c
+++ src/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: ccormon <ccormon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 10:39:37 by ccormon           #+#    #+#             */
-/*   Updated: 2024/06/14 17:32:07 by ccormon          ###   ########.fr       */
+/*   Updated: 2024/06/21 19:19:27 by ccormon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,23 +59,71 @@ bool	is_wall(t_map map, t_point_reel inter)
 
 double	find_hor_inter(t_game *game)
 {
+	t_point_reel	hor_inter;
+
 	if (game->ray.theta < M_PI)
-		game->ray.hor_inter.y = floor(game->map.player.pos.y / CB_SIZE)
+		hor_inter.y = floor(game->map.player.pos.y / CB_SIZE)
 			* CB_SIZE - OFFSET;
 	else
-		game->ray.hor_inter.y = ceil(game->map.player.pos.y / CB_SIZE)
-			* CB_SIZE;
-	game->ray.hor_inter.x = game->map.player.pos.x + (game->ray.hor_inter.y
+		hor_inter.y = ceil(game->map.player.pos.y / CB_SIZE) * CB_SIZE;
+	hor_inter.x = game->map.player.pos.x + (hor_inter.y
 		- game->map.player.pos.y) / tan(game->ray.theta);
-	while (is_on_map(game->map, game->ray.hor_inter)
-		&& !is_wall(game->map, game->ray.hor_inter))
+	while (is_on_map(game->map, hor_inter) && !is_wall(game->map, hor_inter))
 	{
-
+		if (game->ray.theta < M_PI)
+		{
+			hor_inter.x += CB_SIZE / tan(game->ray.theta);
+			hor_inter.y -= CB_SIZE;
+		}
+		else
+		{
+			hor_inter.x -= CB_SIZE / tan(game->ray.theta);
+			hor_inter.y += CB_SIZE;
+		}
 	}
+	if (!is_on_map(game->map, hor_inter))
+		return (INFINITY);
+	else
+		return (sqrt(pow(game->map.player.pos.x - hor_inter.x, 2)
+			+ pow(game->map.player.pos.y - hor_inter.y, 2)));
+}
+
+double	find_ver_inter(t_game *game)
+{
+	t_point_reel	ver_inter;
+
+	if (game->ray.theta >= M_PI_2 && game->ray.theta < 3 * M_PI_2)
+		ver_inter.x = floor(game->map.player.pos.x / CB_SIZE)
+			* CB_SIZE - OFFSET;
+	else
+		ver_inter.x = ceil(game->map.player.pos.x / CB_SIZE)
+			* CB_SIZE;
+	ver_inter.y = game->map.player.y + (ver_inter.x
+		- game->map.player.pos.x) * tan(game->ray.theta);
+	while (is_on_map(game->map, ver_inter) && !is_wall(game->map, ver_inter))
+	{
+		if (game->ray.theta >= M_PI_2 && game->ray.theta < 3 * M_PI_2)
+		{
+			ver_inter.x -= CB_SIZE;
+			ver_inter.y -= CB_SIZE * tan(game->ray.theta);
+		}
+		else
+		{
+			ver_inter.x += CB_SIZE;
+			ver_inter.y += CB_SIZE * tan(game->ray.theta);
+		}
+	}
+	if (!is_on_map(game->map, ver_inter))
+		return (INFINITY);
+	else
+		return (sqrt(pow(game->map.player.pos.x - ver_inter.x, 2)
+			+ pow(game->map.player.pos.y - ver_inter.y, 2)));
 }
 
 void	raycasting(t_game *game)
 {
+	double	hor_inter_len;
+	double	ver_inter_len;
 	int		i;
 	int		j;
 
@@ -84,7 +132,8 @@ void	raycasting(t_game *game)
 	while (i < game->mlx->width)
 	{
 		game->ray.theta = game->map.player.theta + j * M_PI / 30;
-		find_hor_inter(game);
+		hor_inter_len = find_hor_inter(game);
+		ver_inter_len = find_ver_inter(game);
 		i++;
 		j++;
 	}
--- src/rotate.c
+++ src/rotate.c
@@ -6,7 +6,7 @@
 /*   By: ccormon <ccormon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 14:16:58 by ccormon           #+#    #+#             */
-/*   Updated: 2024/06/14 14:32:26 by ccormon          ###   ########.fr       */
+/*   Updated: 2024/06/21 18:21:31 by ccormon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 void	rotate_left(t_game *game)
 {
 	game->map.player.theta -= game->map.player.rotate_speed;
-	while (game->map.player.theta <= -M_PI)
+	while (game->map.player.theta <= 0)
 		game->map.player.theta += 2 * M_PI;
-	while (game->map.player.theta > M_PI)
+	while (game->map.player.theta > 2 * M_PI)
 		game->map.player.theta -= 2 * M_PI;
 }
 
 void	rotate_right(t_game *game)
 {
 	game->map.player.theta += game->map.player.rotate_speed;
-	while (game->map.player.theta <= -M_PI)
+	while (game->map.player.theta <= 0)
 		game->map.player.theta += 2 * M_PI;
-	while (game->map.player.theta > M_PI)
+	while (game->map.player.theta > 2 * M_PI)
 		game->map.player.theta -= 2 * M_PI;
 }
